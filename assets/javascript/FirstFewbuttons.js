57abea781dab493e195b90210adc9ee624c8108f
Anthony Dominguez <Anthonydv7@outlook.com>
2019-08-26 15:33:59 -0500

    buttons when clicked update the page to inculde top 3 drinks and instructions on how to make them

--- assets/javascript/FirstFewbuttons.js
+++ assets/javascript/FirstFewbuttons.js
@@ -1,19 +1,53 @@
-const rum = document.getElementById("");
-const vodka = document.getElementById("");
-const gin = document.getElementById("");
-const tequila = document.getElementById("");
-const cockTailQueryUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink
-const drink = ""
-const rumresults = document.getElementById("");
 
-rum.addEventListener("click", function () {
 
-    const cockTailQueryUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=rum"
-    fetch(cockTailQueryUrl)
-  .then(response => response.json())
-  .then(data => { 
-      for (let i = 0 )
-      rumresults.innerHTML = response.json.drinks[]
-      
+document.querySelectorAll("button").forEach(function (node) {
 
+    node.addEventListener("click", function () {
+
+        const drink = event.target.id
+
+        const cockTailQueryUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink
+
+
+
+        fetch(cockTailQueryUrl).then(function (response) {
+            return response.json()
+        }).then(function (responseJson) {
+            const results = responseJson
+
+            for (let i = 0; i < 3; i++) {
+
+
+                const cockTail = results.drinks[i].strDrink;
+
+                const recipeInstructions = results.drinks[i].strInstructions;
+
+                const drinkDiv = document.createElement("div");
+                drinkDiv.setAttribute("id","container");
+
+                const cockTailDiv = document.createElement("p");
+                cockTailDiv.setAttribute("id","name");
+                cockTailDiv.innerHTML = cockTail;
+
+                const recipeDiv = document.createElement("p");
+                recipeDiv.setAttribute("id","instructions");
+                recipeDiv.innerHTML = recipeInstructions;
+                
+                drinkDiv.append(cockTailDiv);
+
+                drinkDiv.append(recipeDiv);
+                document.getElementById("drink-names").prepend(recipeDiv);
+                document.getElementById("drink-names").prepend(cockTailDiv);
+               
+                
+                console.log(cockTail)
+                console.log(recipeInstructions)
+
+            }
+            
+
+        });
+
+
+    });
 });
