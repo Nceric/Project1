91f70b01fade91c55f055fcaf891a3814ddeb14e
Anthony Dominguez <Anthonydv7@outlook.com>
2019-08-29 18:34:09 -0500

    page 1 is almost done

--- assets/javascript/FirstFewButtons.js
+++ assets/javascript/FirstFewButtons.js
@@ -18,21 +18,34 @@ document.querySelectorAll("button").forEach(function (node) {
 
                 const cockTail = results.drinks[i].strDrink;
                 const recipeInstructions = results.drinks[i].strInstructions;
+                const imgURL = responseJson.drinks[i].strDrinkThumb;
 
                 const drinkDiv = document.createElement("div");
-                drinkDiv.setAttribute("id","container");
+                drinkDiv.setAttribute("id", "container");
+
+                const recipeContainerDiv = document.createElement("div");
+                recipeContainerDiv.setAttribute("id", "instructions-container");
+
+                const imageContainerDiv = document.createElement("div");
+                imageContainerDiv.setAttribute("id", "image-container");
 
                 const cockTailDiv = document.createElement("p");
-                cockTailDiv.setAttribute("id","name");
+                cockTailDiv.setAttribute("id", "name");
                 cockTailDiv.innerHTML = cockTail;
 
-                const recipeDiv = document.createElement("p");
-                recipeDiv.setAttribute("id","instructions");
-                recipeDiv.innerHTML = recipeInstructions;
-                
-                drinkDiv.append(cockTailDiv,recipeDiv);
+                const imageDiv = document.createElement("img");
+                imageDiv.setAttribute("src", imgURL);
+                imageDiv.innerHTML = imgURL;
 
-                document.getElementById("drink-names").prepend(recipeDiv,cockTailDiv);
+                const recipeDiv = document.createElement("p");
+                recipeDiv.setAttribute("id", "instructions");
+                recipeDiv.innerHTML = recipeInstructions;
+
+                drinkDiv.append(cockTailDiv);
+                recipeContainerDiv.append(recipeDiv);
+                imageContainerDiv.append(imageDiv);
+
+                document.getElementById("drink-names").prepend(cockTailDiv,imageDiv, recipeDiv);            
             }
         });
     });
