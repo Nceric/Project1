8a5a075d0164ba749d2e9723160f26ff3415c87e
Anthony Dominguez <Anthonydv7@outlook.com>
2019-09-04 14:55:47 -0500

    nutrient information is now working and presented on the dom

--- assets/javascript/FirstFewButtons.js
+++ assets/javascript/FirstFewButtons.js
@@ -5,22 +5,50 @@ const getIngredients = obj => {
 
     const keys = Object.keys(obj).filter(key => key.indexOf(re) > -1);
 
-    const filter =  keys.map(key => obj[key]).filter(ingred => ingred !== '' && ingred !== " ");
+    const filter = keys.map(key => obj[key]).filter(ingred => ingred !== '' && ingred !== " ");
 
     const ingredientsList = document.createElement("UL");
 
-    for (let i =0; i<filter.length; i++){
 
-        const filteredList = filter[i];
+    for (let i = 0; i < filter.length; i++) {
+        const foodItem = filter[i]
 
-        const filteredListItem = document.createElement("li");
+        const apiKey = "ebeBxEQ0lUv4b4YUNMdwXZyH6sPjKF8Fbg6xdwWp"
+        const url = "https://api.nal.usda.gov/ndb/search/?format=json&q=" + foodItem + "&max=1&api_key=" + apiKey;
 
-        filteredListItem .textContent = filteredList;
+        fetch(url).then(response => {
+            return response.json()
+        }).then(responseJson => {
+            console.log(responseJson.list.item[0].ndbno)
+            const secondUrl = "https://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=" + apiKey + "&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=" + responseJson.list.item[0].ndbno;
 
-        ingredientsList.append(filteredListItem)
+            fetch(secondUrl).then(response => {
+                return response.json()
+            }).then(responseJson => {
+                console.log(responseJson.report.foods[0].nutrients)
+                const foodItem = filter[i]
+
+                const drinkCalories = responseJson.report.foods[0].nutrients[0].value
+
+                console.log(drinkCalories)
+               
+                const filteredList = filter[i];
+
+                const filteredListItem = document.createElement("li");
+
+                filteredListItem.textContent = filteredList;
+
+                ingredientsList.append(filteredListItem,drinkCalories)
+
+
+                
+                console.log(foodItem)
+            })
+
+
+        });
 
     }
-
     return ingredientsList;
 
 }
@@ -45,8 +73,8 @@ document.querySelectorAll("button").forEach(node => {
             let wrapper;
 
             for (let i = 0; i < 3; i++) {
-                
-             
+
+
 
                 wrapper = document.createElement('div');
                 wrapper.classList.add('drink-wrapper');
@@ -54,6 +82,9 @@ document.querySelectorAll("button").forEach(node => {
                 const cockTailName = results.drinks[i].strDrink;
                 const recipeInstructions = results.drinks[i].strInstructions;
                 const imgURL = responseJson.drinks[i].strDrinkThumb;
+
+
+
 
                 const nameRecipeWrapper = document.createElement('div');
                 nameRecipeWrapper.classList.add('name-recipe-wrapper');
@@ -74,12 +105,13 @@ document.querySelectorAll("button").forEach(node => {
                 ingredientsDiv.setAttribute("id", "ingredients-list");
 
                 const ingredients = getIngredients(results.drinks[i]);
-                console.log(ingredients);
+
+
 
                 const ingredientsList = document.createElement("UL");
                 ingredientsList.innerHTML = ingredients;
 
-                
+
 
                 ingredientsDiv.append(ingredients);
                 nameRecipeWrapper.append(cockTailDiv, ingredientsDiv);
