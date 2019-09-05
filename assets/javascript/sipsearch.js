34b06a5678720f6130ab33b7906b5664cc13f1f7
zeftambornino@gmail.com <zeftambornino@gmail.com>
2019-09-05 09:30:30 -0500

    fixed typos and formatted code

--- assets/javascript/sipsearch.js
+++ assets/javascript/sipsearch.js
@@ -1,15 +1,12 @@
 const getIngredients = obj => {
 
     const re = 'strIngredient';
-
     const keys = Object.keys(obj).filter(key => key.indexOf(re) > -1);
-
     const filters = keys.map(key => obj[key]).filter(ingred => ingred !== '' && ingred !== " ");
-
     const ingredientsList = document.createElement("UL");
 
 
-        _.forEach (filters, function(foodItem){
+    _.forEach(filters, function (foodItem) {
 
         const apiKey = "ebeBxEQ0lUv4b4YUNMdwXZyH6sPjKF8Fbg6xdwWp"
         const url = "https://api.nal.usda.gov/ndb/search/?format=json&q=" + foodItem + "&max=1&api_key=" + apiKey;
@@ -26,97 +23,72 @@ const getIngredients = obj => {
                 console.log(responseJson.report.foods[0].nutrients)
 
                 const drinkCalories = "kCal: " + responseJson.report.foods[0].nutrients[0].value
-
                 console.log(drinkCalories)
-
                 const filteredList = foodItem;
-
                 const filteredListItem = document.createElement("li");
-
                 filteredListItem.textContent = filteredList;
-
                 ingredientsList.append(filteredListItem, drinkCalories)
-
-
-
                 console.log(foodItem)
             })
-
-
         });
-
     })
     return ingredientsList;
-
 }
 
 
 document.querySelectorAll("button").forEach(node => {
 
-            node.addEventListener("click", () => {
+    node.addEventListener("click", () => {
 
-                    const drink = event.target.id;
+        const drink = event.target.id;
+        const cockTailQueryUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
 
-                    const cockTailQueryUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
+        fetch(cockTailQueryUrl).then(response => {
+            return response.json()
+        }).then(responseJson => {
+            const responseDrinks = responseJson.drinks;
+            const drinks = _.dropRight(responseDrinks, responseDrinks.length - 3);
 
-                    fetch(cockTailQueryUrl).then(response => {
-                        return response.json()
-                    }).then(responseJson => {
-                            const responseDrinks = responseJson.drinks;
-                            const drinks = _.dropRight(responseDrinks, responseDrinks.length - 3);
+            document.getElementById("drink-names").innerHTML = '';
+            let wrapper;
 
-                            document.getElementById("drink-names").innerHTML = '';
+            _.forEach(drinks, function (drink) {
 
-                            let wrapper;
+                wrapper = document.createElement('div');
+                wrapper.classList.add('drink-wrapper');
 
-                            _.forEach(drinks, function (drink) {
+                const cockTailName = drink.strDrink;
+                const recipeInstructions = drink.strInstructions;
+                const imgURL = drink.strDrinkThumb;
 
+                const nameRecipeWrapper = document.createElement('div');
+                nameRecipeWrapper.classList.add('name-recipe-wrapper');
 
+                const cockTailDiv = document.createElement("p");
+                cockTailDiv.setAttribute("id", "name");
+                cockTailDiv.innerHTML = cockTailName;
 
-                                    wrapper = document.createElement('div');
-                                    wrapper.classList.add('drink-wrapper');
+                const imageDiv = document.createElement("img");
+                imageDiv.setAttribute("src", imgURL);
+                imageDiv.innerHTML = imgURL;
 
-                                    const cockTailName = drink.strDrink;
-                                    const recipeInstructions = drink.strInstructions;
-                                    const imgURL = drink.strDrinkThumb;
+                const recipeDiv = document.createElement("p");
+                recipeDiv.setAttribute("id", "instructions");
+                recipeDiv.innerHTML = recipeInstructions;
 
+                const ingredientsDiv = document.createElement("p");
+                ingredientsDiv.setAttribute("id", "ingredients-list");
 
+                const ingredients = getIngredients(drink);
+                const ingredientsList = document.createElement("UL");
+                ingredientsList.innerHTML = ingredients;
 
+                ingredientsDiv.append(ingredients);
+                nameRecipeWrapper.append(cockTailDiv, ingredientsDiv);
+                wrapper.append(nameRecipeWrapper, recipeDiv, imageDiv);
 
-                                    const nameRecipeWrapper = document.createElement('div');
-                                    nameRecipeWrapper.classList.add('name-recipe-wrapper');
-
-                                    const cockTailDiv = document.createElement("p");
-                                    cockTailDiv.setAttribute("id", "name");
-                                    cockTailDiv.innerHTML = cockTailName;
-
-                                    const imageDiv = document.createElement("img");
-                                    imageDiv.setAttribute("src", imgURL);
-                                    imageDiv.innerHTML = imgURL;
-
-                                    const recipeDiv = document.createElement("p");
-                                    recipeDiv.setAttribute("id", "instructions");
-                                    recipeDiv.innerHTML = recipeInstructions;
-
-                                    const ingredientsDiv = document.createElement("p");
-                                    ingredientsDiv.setAttribute("id", "ingredients-list");
-
-                                    const ingredients = getIngredients(drink);
-
-
-
-                                    const ingredientsList = document.createElement("UL");
-                                    ingredientsList.innerHTML = ingredients;
-
-
-
-                                    ingredientsDiv.append(ingredients);
-                                    nameRecipeWrapper.append(cockTailDiv, ingredientsDiv);
-                                    wrapper.append(nameRecipeWrapper, recipeDiv, imageDiv);
-
-                                    document.getElementById("drink-names").prepend(wrapper);
-
-                                });
-                            });
-                    });
+                document.getElementById("drink-names").prepend(wrapper);
             });
+        });
+    });
+});
